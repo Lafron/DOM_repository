d757df74db4a389f1246e60980dfae605c3b2e86
Epshtein Dmitry <letuver@yahoo.com>
2021-12-10 13:05:34 +0200

    change script

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-const btn = document.querySelector("button");
-const textBox = document.querySelector("input[type='text']"); 
+const btn = document.querySelector("#btn");
+const textBox = document.querySelector("input[type='text']");
+const square = document.querySelector("#square");
 const bttn = document.querySelector("#e_btn");
 const range = document.querySelector("#range");
 const circle = document.querySelector("#circle");
+const rangeSpan = document.getElementById("range-span");
 
 btn.addEventListener("click", handler);
 
 function handler(){
   let color = textBox.value;
   
-  if((color == null)||(color == "")){
+  if(color == ""){
     alert("Enter valid color");
   }
   else{
-    const square = document.querySelector("#square");
-    console.log(square);
     square.style.backgroundColor = color;
-    
     textBox.value = "";
   }
-    
 }
 
 bttn.style.display = "none";
 
 range.addEventListener("change", resize);
 
+
 function resize(){
-    console.log(range.value);
-
-    circle.style.width = range.value + "%";
-    circle.style.height = range.value + "%";
-
+  rangeSpan.innerText = range.value;
+  circle.style.width = range.value + "%";
+  circle.style.height = range.value + "%";
 }
